85f71facafa8b1f9753c74d196450ed73222c75c
UnemployCoin™ <unemploycoin@gmail.com>
2025-07-03 08:51:28 -0500

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://unemploycoin.com'
+}));
+
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
