c8ef6aceb536c7914f8928281c79a8a1313ca684
UnemployCoin™ <unemploycoin@gmail.com>
2025-07-03 10:50:41 -0500

    Update server.js

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.post('/api/ask', async (req, res) => {
         "X-Title": "UnemployCoinChatBot"
       },
       body: JSON.stringify({
-        model: "deepseek/deepseek-coder-v3:free",
+        model: "deepseek/deepseek-chat-v3-0324:free"",
         messages: [
   {
     role: "system",
